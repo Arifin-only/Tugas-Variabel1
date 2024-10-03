7a79fb16288b291826efffbb8709a92b1f1a17a8
Arifin-only <arifin180104@gmail.com>
2024-10-03 13:45:57 +0800

    Create kata_kunci_finall.dart

---
+++ kata_kunci_finall.dart
@@ -0,0 +1,11 @@
+void main(){
+    var namapertama = "Muhammad";
+    final namaterakhir = "Arifin";
+    
+    namapertama = "Muhammad Arifin";
+    namaterakhir = "Ahmad Arifin";
+    
+    print (namapertama);
+    print (namaterakhir);
+    
+}
