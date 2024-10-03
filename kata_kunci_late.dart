2b843105a34cd4dca583655aa205059cb75801c5
Arifin-only <arifin180104@gmail.com>
2024-10-03 14:17:23 +0800

    Create kata_kunci_late.dart

---
+++ kata_kunci_late.dart
@@ -0,0 +1,10 @@
+void main(){
+    late var value = getValue();
+    print("Arifin");
+    print(value);
+    }
+    String getValue(){
+        print("panggil value");
+        return "Arifin";
+    }
+    
