76037e650e97bb96c03a941887f6f1799a12943d
Arifin-only <arifin180104@gmail.com>
2024-10-03 14:26:55 +0800

    Create komentar.dart

---
+++ komentar.dart
@@ -0,0 +1,14 @@
+/// ini ada adalah documentation
+/// yang biasanya aka digenerate menjadi doc
+void main(){
+    //this is variabel
+    
+    var name = "M.Arifin";
+    
+    /**
+     * ini adalah komentar
+     * lebih dari satu baris
+     */
+     print(name);
+    }
+    
